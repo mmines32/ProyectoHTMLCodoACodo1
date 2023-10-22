346b30e137a878c39e0500b70d42aa0a44fab28d
ines <inesmanriqv@gmail.com>
2023-10-22 02:14:13 -0300

    Arreglos en el formulario

--- js/form.js
+++ js/form.js
@@ -1,21 +1,20 @@
 const btn = document.getElementById('submit')
 const name = document.getElementById('name');
-const password1 = document.getElementById('password1');
-const password2 = document.getElementById('password2');
-const gender = document.getElementsByName('gender');
+const email = document.getElementById('email');
+const comentario = document.getElementById('comentario');
+
 
 
 const error_name = document.getElementById('error-name');
-const error_password1 = document.getElementById('error-password1');
-const error_password2 = document.getElementById('error-password2');
-const error_gender = document.getElementById('error-gender');
+const error_email = document.getElementById('error-email');
+const error_comentario = document.getElementById('error-comentario');
 
 
-var error = [];
+let error = [];
 
 function validateName() {
 	if (!name.value) {
-		 error_name.innerHTML = `<p>Por favor, insira um nome!</p>`;
+		 error_name.innerHTML = `<p>Escriba su nombre y apellido!</p>`;
 	}
 	else {
 		 error_name.innerHTML = ``;
@@ -24,70 +23,39 @@ function validateName() {
 
 }
 
-function validatePassword1() {
+function validateEmail() {
 
-	if (!password1.value) {
-		error_password1.innerHTML = `<p>Por favor, insgresa una contraseña!</p>`
+	if (!email.value) {
+		error_email.innerHTML = `<p>Por favor, ingrese su email!</p>`
 	} else {
-		error_password1.innerHTML = ``
+		error_email.innerHTML = ``
 
 	}
-	validatePassword();
 }
 
-function validatePassword2() {
-	if (!password2.value) {
-		error_password2.innerHTML = `<p>Por favor, confirme su contraseña!</p>`
+function validateComentario() {
+	if (!comentario.value) {
+		error_comentario.innerHTML = `<p>Por favor, ingrese un comentario!</p>`
 	} else {
-		error_password2.innerHTML = ``
+		error_comentario.innerHTML = ``
 
-	}
-	validatePassword();
-
-}
-
-function validatePassword() {
-	if (password1.value && password2.value) {
-		if (password1.value != password2.value) {
-			
-			 error_password2.innerHTML = `<p>Su contraseña  no coincide!</p>`;
-		}
-		return true;
-	
 	}
 }
 
-function validateChecked() {
-	var isChecked = false;
-	for (var i = 0; i < gender.length; i++) {
-		if (gender[i].checked) {
-			isChecked = true;
-			break;
-		}
-	}
-	if (!isChecked) {
-		error_gender.innerHTML = `<p>Por favor, selecione un sexo.</p>`
-	
-	} else {
-		error_gender.innerHTML = ``
-	}
-
-
-}
-
-function validate(){
+function validate() {
 	validateName();
-	validatePassword1();
-	validatePassword2();
-	validateChecked();
+	validateEmail();
+	validateComentario();
+
 	console.log(validateName())
-	console.log(validatePassword())
-	
-	if(validateName() && validatePassword() === true){
+	console.log(validateEmail())
+	console.log(validateComentario())
+
+	if (validateName() && validateEmail() && validateComentario()===true) {
 		btn.style.display = "flex";
+	
 	}
 }
-
 
 
 btn.onclick = function submit() {
